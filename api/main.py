3c5d35a7521bafd197690dc3c5fda171becf3eea
Livingstone Eli Ayivor <103898284+EliEli94@users.noreply.github.com>
2024-10-15 11:25:21 +0000

    frontend

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -6,6 +7,19 @@ from PIL import Image
 import tensorflow as tf
 
 app =FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 MODEL = tf.keras.models.load_model('saved_models/2.keras')
 CLASS_NAME  = ["Early Blight", "Late Blight", "Healthy"]
